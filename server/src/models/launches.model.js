2999ff23ef3f10917179bb085345574e484d2f76
v.finchuk <finchukvaldimir@gmail.com>
2021-08-31 21:31:54 +0300

    [NASA_1] Create launches model

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,19 @@
+
+const launches = new Map();
+
+const launch = {
+  launchNumber: 100,
+  mission: 'Kepler exploration X',
+  rocket: 'Explorer IS1',
+  launchDate: new Date('December 27, 2030'),
+  destination: 'Kepler-442 b',
+  customers: ['NASA', 'ZTM'],
+  upcoming: true,
+  success: true,
+}
+
+launches.set(launch.launchNumber, launch);
+
+module.exports = {
+  launches,
+};
