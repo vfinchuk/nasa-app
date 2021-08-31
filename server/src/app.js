5899fac66535530b688765b23c678459b2bdaab7
v.finchuk <finchukvaldimir@gmail.com>
2021-08-31 20:30:24 +0300

    [NASA_1] Add deploy script. Add to use in express static index.html

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,19 @@
-const express = require('express');
+const path = require('path');
 const cors = require('cors');
+const express = require('express');
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
 app.use(cors({
   origin: 'http://localhost:3000',
 }));
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(express.json());
 app.use(planetsRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app;
