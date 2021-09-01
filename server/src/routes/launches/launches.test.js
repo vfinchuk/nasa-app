1d0795ad9af02920869205fd2d849985aa6f9e73
v.finchuk <finchukvaldimir@gmail.com>
2021-09-02 00:05:39 +0300

    [NASA_1] Install jest and init launches test file

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,20 @@
+ describe('Test GET/launches', () => {
+   test('It should response with 200 success', () => {
+     const response = 200;
+     expect(response).toBe(200);
+   })
+ });
+
+describe('Test POST/launch', () => {
+  test('It should response with 200 success', () => {
+
+  });
+
+  test('It should catch missing launch properties', () => {
+
+  });
+
+  test('It should catch invalid launch date', () => {
+
+  });
+});
