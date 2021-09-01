ffca9c8d8d95edae37d6bec0c1cbce1f4033f706
v.finchuk <finchukvaldimir@gmail.com>
2021-09-01 16:59:17 +0300

    [NASA_1] Update fetching launches func.

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,15 +1,14 @@
-
 const API_URL = 'http://localhost:8000';
 
 async function httpGetPlanets() {
   const response = await fetch(`${API_URL}/planets`);
   return await response.json();
-  // Load planets and return as JSON.
 }
 
 async function httpGetLaunches() {
-  // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
+  const response = await fetch(`${API_URL}/launches`);
+  const fetchedLaunches = await response.json();
+  return fetchedLaunches.sort((a, b) => a.launchNumber - b.launchNumber);
 }
 
 async function httpSubmitLaunch(launch) {
