8261b91a348f0eda303e1958f656543d8b5066b6
v.finchuk <finchukvaldimir@gmail.com>
2021-09-01 16:44:16 +0300

    [NASA_1] Add launches route

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
 const planetsRouter = require('./routes/planets/planets.router');
+const launchesRouter = require('./routes/launches/launches.router');
 
 const app = express();
 app.use(cors({
@@ -14,6 +15,7 @@ app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, '..', 'public')))
 app.use(express.json());
 app.use(planetsRouter);
+app.use(launchesRouter);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
